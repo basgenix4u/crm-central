76e77e68b06342e14351f6ab63e196a32cd0b6c1
basgenix4u <basgenix4u@users.noreply.github.com>
2026-06-07 01:09:07 +0000

    fix: Exclude mail auto-config, further reduce JVM memory for 512MB container

--- backend/src/main/java/com/crmcentral/CrmCentralApplication.java
+++ backend/src/main/java/com/crmcentral/CrmCentralApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,7 +12,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {
     RedisAutoConfiguration.class,
-    RedisRepositoriesAutoConfiguration.class
+    RedisRepositoriesAutoConfiguration.class,
+    MailSenderAutoConfiguration.class
 })
 @EnableJpaAuditing
 @EnableCaching
